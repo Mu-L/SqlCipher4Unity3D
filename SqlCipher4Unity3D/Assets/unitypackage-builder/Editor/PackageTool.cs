89c8087947c2de8fa212e9d8f719bce64adda6df
netpyoung <netpyoung@gmail.com>
2025-12-20 02:41:59 +0900

    SqlCipher4Unity3D 1.3.6 - (libsqlcipher version v4.10.0)

--- SqlCipher4Unity3D/Assets/unitypackage-builder/Editor/PackageTool.cs
+++ SqlCipher4Unity3D/Assets/unitypackage-builder/Editor/PackageTool.cs
@@ -5,7 +5,7 @@ public class PackageTool
     [MenuItem("Package/Update Package")]
     private static void UpdatePackage()
     {
-        const string version = "1.3.5";
+        const string version = "1.3.6";
         AssetDatabase.ExportPackage(new string[] { "Assets/SqlCipher4Unity3D" }, $"../SqlCipher4Unity3D-{version}.unitypackage", ExportPackageOptions.Recurse);
     }
 }
